10dffa74f25096f6d5582f19db655e7ac907b6b7
Aidana <aidana.khassenova@tele2.kz>
2024-04-01 09:36:56 +0500

    adding files

---
+++ README.md
@@ -0,0 +1,2 @@
+# postulat
+Postulat Design System
---
+++ build/ios-swift/StyleDictionaryFont.swift
@@ -0,0 +1,29 @@
+
+//
+// StyleDictionaryFont.swift
+//
+
+// Do not edit directly
+// Generated on Mon, 01 Apr 2024 01:20:38 GMT
+
+
+import SwiftUI
+
+public enum StyleDictionaryFont {
+    public static let TypeDisplayLarge = [object Object]
+    public static let TypeDisplayMedium = [object Object]
+    public static let TypeDisplaySmall = [object Object]
+    public static let TypeDisplayXsmall = [object Object]
+    public static let TypeHeadingLarge = [object Object]
+    public static let TypeHeadingMedium = [object Object]
+    public static let TypeHeadingSmall = [object Object]
+    public static let TypeHeadingXsmall = [object Object]
+    public static let TypeLabelLarge = [object Object]
+    public static let TypeLabelMedium = [object Object]
+    public static let TypeLabelSmall = [object Object]
+    public static let TypeLabelXsmall = [object Object]
+    public static let TypeParagraphLarge = [object Object]
+    public static let TypeParagraphMedium = [object Object]
+    public static let TypeParagraphSmall = [object Object]
+    public static let TypeParagraphXsmall = [object Object]
+}
